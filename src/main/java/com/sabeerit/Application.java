9eea494e5de13fb441801764e2aad0c84406ecc3
sabeer-hussain <msabeerhussain.edu@gmail.com>
2021-02-06 10:28:59 +0530

    Main class modified

--- src/main/java/com/sabeerit/Application.java
+++ src/main/java/com/sabeerit/Application.java
@@ -2,12 +2,15 @@ package com.sabeerit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		
+		context.close();
 	}
 
 }
