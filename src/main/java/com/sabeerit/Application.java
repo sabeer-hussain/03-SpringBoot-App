fc5f169cc5a10df7536463808533ea9a6d09edf7
sabeer-hussain <msabeerhussain.edu@gmail.com>
2021-02-06 12:29:54 +0530

    HIS-134 fix provided

--- src/main/java/com/sabeerit/Application.java
+++ src/main/java/com/sabeerit/Application.java
@@ -9,7 +9,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-		
+		//changes for HIS-134
 		context.close();
 	}
 
